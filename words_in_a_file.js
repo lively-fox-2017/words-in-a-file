c03858f5f77c8ed3490bd9449f9000fb1b6aeca8
NNcrawler <rasyid.xyz@gmail.com>
2017-09-21 13:07:04 +0700

    release 1 done without ipsum generator

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -5,11 +5,18 @@ const fs = require('fs')
 function words_in_a_file(filename, limit) {
   var data = fs.readFileSync(filename);
   var counterWords = {}
+  var stopWords = ['a','the', 'and', 'of', 'in', 'to']
   //console.log(data.toString().split(' '));
+  var result ='';
+
   var stringForm = data.toString();
   var stringFormNoTandaBaca=stringForm.replace(/[^0-9 a-z A-Z]/g, " ");
   var stringFormCleanWS = stringFormNoTandaBaca.replace(/\s\s+/g, ' ');
   var arrCleanStr = stringFormCleanWS.split(' ');
+
+  arrCleanStr = arrCleanStr.filter((item) => {if (stopWords.indexOf(item)==-1){return item}});
+  //console.log(arrCleanStr);
+
   for (var i of arrCleanStr) {
     //kalo belum ada di counter add property
     if(counterWords[i]==undefined){
@@ -26,15 +33,29 @@ function words_in_a_file(filename, limit) {
     arrPenampungJumlahCounter.push(counterWords[i]);
   }
   arrPenampungJumlahCounter.sort(function(a,b){return a-b});
+  result+=limit+' kata terbanyak\n';
   for(var j=0; j<limit; j++){
     for (var i in counterWords) {
       if (counterWords[i]==arrPenampungJumlahCounter[arrPenampungJumlahCounter.length-1]) {
-        console.log(i);
+        result+=i+' : ';
       }
     }
-    console.log(arrPenampungJumlahCounter.pop());
+    result+=arrPenampungJumlahCounter.pop()+'\n';
   }
+  //kata terjarang
 
+  result+= '\nKata paling sedikit digunakan :\n';
+
+  for (var i in counterWords) {
+    if (counterWords[i]==arrPenampungJumlahCounter[0]) {
+      result+=i+' : ';
+
+    }
+  }
+  result+=arrPenampungJumlahCounter.shift()+'\n';
+
+
+  return result;
   //console.log(counterWordsarrPenampungJumlahCounter);
   //hapus semua tanda baca dan ganti dengan whitespace
   //setelah semua tanda baca hilang lakukan perhitungan jumlah kata
